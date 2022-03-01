rmandvikar/masking

nuget: Nugetize (pack, push, delete)

nuget-delete.sh
@@ -1,0 +1,17 @@
+#!/bin/sh
+
+#usage: nuget-delete <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+dotnet nuget delete rm.Masking "$version" \
+	-k $(< ~/dump/.nuget.apikey) \
+	-s https://api.nuget.org/v3/index.json \
+	--non-interactive

nuget-pack.sh
@@ -1,0 +1,24 @@
+#!/bin/sh
+
+#usage: nuget-pack <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+tag="nuget-$version"
+commit_hash=$(git rev-parse @)
+metadata="$commit_hash"
+
+dotnet pack src/rm.Masking/rm.Masking.csproj \
+	-c Release \
+	-o .nupkg/ \
+	//p:Version="$version+$metadata" \
+	//p:PackageVersion="$version+$metadata" \
+	//p:PackageReleaseNotes="tag: $tag" \
+	&& git tag "$tag" -m "Create nuget tag $tag"

nuget-push.sh
@@ -1,0 +1,19 @@
+#!/bin/sh
+
+#usage: nuget-push <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+tag="nuget-$version"
+
+dotnet nuget push .nupkg/rm.Masking."$version".nupkg \
+	-k $(< ~/dump/.nuget.apikey) \
+	-s https://api.nuget.org/v3/index.json \
+	&& git push $(git remote) "$tag"

rmandvikar/threading

nuget-delete.sh
@@ -1,0 +1,17 @@
+#!/bin/sh
+
+#usage: nuget-delete <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+dotnet nuget delete rm.Threading "$version" \
+	-k $(< ~/dump/.nuget.apikey) \
+	-s https://api.nuget.org/v3/index.json \
+	--non-interactive

nuget-pack.sh
@@ -1,0 +1,24 @@
+#!/bin/sh
+
+#usage: nuget-pack <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+tag="nuget-$version"
+commit_hash=$(git rev-parse @)
+metadata="$commit_hash"
+
+dotnet pack src/rm.Threading/rm.Threading.csproj \
+	-c Release \
+	-o .nupkg/ \
+	//p:Version="$version+$metadata" \
+	//p:PackageVersion="$version+$metadata" \
+	//p:PackageReleaseNotes="tag: $tag" \
+	&& git tag "$tag" -m "Create nuget tag $tag"

nuget-push.sh
@@ -1,0 +1,19 @@
+#!/bin/sh
+
+#usage: nuget-push <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+tag="nuget-$version"
+
+dotnet nuget push .nupkg/rm.Threading."$version".nupkg \
+	-k $(< ~/dump/.nuget.apikey) \
+	-s https://api.nuget.org/v3/index.json \
+	&& git push $(git remote) "$tag"