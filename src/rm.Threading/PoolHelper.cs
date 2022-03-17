dfde98cddcd9c0e78c533e12b8d5ff5750a915b3
hippy <hippy2981@gmail.com>
2022-03-17 00:46:49 -0700

    pool: Tweak pool size function, and min pool size

--- src/rm.Threading/PoolHelper.cs
+++ src/rm.Threading/PoolHelper.cs
@@ -11,11 +11,11 @@ namespace rm.Threading
 		/// Size of the pool.
 		/// <para></para>
 		/// Environment.ProcessorCount is not always correct so use more slots as buffer,
-		/// with a minimum of 32 slots.
+		/// with a minimum of 64 slots.
 		/// <para></para>
 		/// Note: Trick borrowed from LazyCache impl.
 		/// </summary>
-		public static readonly int PoolSize = Math.Max(Environment.ProcessorCount << 3, 32);
+		public static readonly int PoolSize = Math.Max(Environment.ProcessorCount << 6, 64);
 
 		/// <summary>
 		/// Returns an index from the pool that the <paramref name="key"/> maps to.
